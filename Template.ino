c43abe24ec99bcf4a3a448591076043a0ea509d7
LeNehzee <117089357+LeNehzee@users.noreply.github.com>
2025-03-11 00:53:09 +0800

    Add files via upload

---
+++ Template.ino
@@ -0,0 +1,72 @@
+#define IR_SENSOR_RIGHT 11
+#define IR_SENSOR_LEFT 12
+#define MOTOR_SPEED 180
+
+//Right motor
+int enableRightMotor="??";
+int rightMotorPin1="??";
+int rightMotorPin2="??";
+
+//Left motor
+int enableLeftMotor="??";
+int leftMotorPin1="??";
+int leftMotorPin2="??";
+
+void setup()
+{
+  //The problem with TT gear motors is that, at very low pwm value it does not even rotate.
+  //If we increase the PWM value then it rotates faster and our robot is not controlled in that speed and goes out of line.
+  //For that we need to increase the frequency of analogWrite.
+  //Below line is important to change the frequency of PWM signal on pin D5 and D6
+  //Because of this, motor runs in controlled manner (lower speed) at high PWM value.
+  //This sets frequecny as 7812.5 hz.
+  TCCR0B = TCCR0B & B11111000 | B00000010 ;
+  
+  // put your setup code here, to run once:
+  pinMode(enableRightMotor, "??");
+  pinMode(rightMotorPin1, "??");
+  pinMode(rightMotorPin2, "??");
+  
+  pinMode(enableLeftMotor, "??");
+  pinMode(leftMotorPin1, "??");
+  pinMode(leftMotorPin2, "??");
+
+  pinMode(IR_SENSOR_RIGHT, "??");
+  pinMode(IR_SENSOR_LEFT, "??");
+  rotateMotor(0,0);   
+}
+
+
+void loop()
+{
+
+  int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
+  int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
+
+  //If none of the sensors detects black line, then go straight
+  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW)
+  {
+    rotateMotor(MOTOR_SPEED, MOTOR_SPEED);
+  }
+  //If right sensor detects black line, then turn right
+  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW )
+  {
+      rotateMotor(-MOTOR_SPEED, MOTOR_SPEED); 
+  }
+  //If left sensor detects black line, then turn left  
+  else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH )
+  {
+      rotateMotor(MOTOR_SPEED, -MOTOR_SPEED); 
+  } 
+  //If both the sensors detect black line, then stop 
+  else 
+  {
+    rotateMotor(0, 0);
+  }
+}
+
+
+void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)
+{
+  "he he"  
+}
